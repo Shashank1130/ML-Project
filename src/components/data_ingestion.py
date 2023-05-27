4c79143360adeb5ee1f3b5f277172dc115568a7e
Shashank Dubey <shashankdubey3420@gmail.com>
2023-05-27 21:54:10 +0530

    EDA and Problem Statement

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,4 @@
+# component folder:- these components are like all the modules taht we are probably going to create like initially we will create 'DATA INGESTION', 'DATA VALIDATION', 'DATA TRANSFROMATION', etc.
+
+
+# This will have all code that is something related to reading the data
